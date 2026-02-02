3e27e078e49163b860279c6d292bc3183b91900b
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2026-02-02 22:18:02 +0100

    _CCCL_GRID_CONSTANT

--- cub/cub/device/dispatch/kernels/kernel_transform.cuh
+++ cub/cub/device/dispatch/kernels/kernel_transform.cuh
@@ -1013,9 +1013,9 @@ template <typename PolicySelector,
   requires transform_policy_selector<PolicySelector>
 #endif // _CCCL_HAS_CONCEPTS()
 __launch_bounds__(get_block_threads<PolicySelector>) CUB_DETAIL_KERNEL_ATTRIBUTES void transform_kernel(
-  Offset num_items,
-  int num_elem_per_thread,
-  [[maybe_unused]] bool can_vectorize,
+  _CCCL_GRID_CONSTANT const Offset num_items,
+  _CCCL_GRID_CONSTANT const int num_elem_per_thread,
+  _CCCL_GRID_CONSTANT [[maybe_unused]] const bool can_vectorize,
   Predicate pred,
   F f,
   RandomAccessIteratorOut out,
